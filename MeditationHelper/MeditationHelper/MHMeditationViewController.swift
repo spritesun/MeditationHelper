9c82ee42f999a3d320b8420a3fd5adad8cb07dad
Long Sun <spritesun@gmail.com>
2015-01-12 16:18:13 +1100

    register alarm notification, replace assets

--- MeditationHelper/MeditationHelper/MHMeditationViewController.swift
+++ MeditationHelper/MeditationHelper/MHMeditationViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class MHMeditationViewController: UIViewController {
   
+  @IBOutlet var datePicker: UIDatePicker!
+  
+  @IBOutlet var controlButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   @IBAction func start(sender: AnyObject) {
-//    let saveRecordTVC = self.storyboard?.instantiateViewControllerWithIdentifier("MHSaveRecordTableViewController") as UIViewController
-
-    let saveRecordNC = self.storyboard?.instantiateViewControllerWithIdentifier("MHSaveRecordNavigationController") as UINavigationController
-//    saveRecordNC.pushViewController(saveRecordTVC, animated: false)
-
-    self.presentViewController(saveRecordNC, animated: true, completion: nil)
+    UIApplication.sharedApplication().cancelAllLocalNotifications()
+    self.controlButton.selected = !self.controlButton.selected
+    self.registerNotification()
+    //    let saveRecordNC = self.storyboard?.instantiateViewControllerWithIdentifier("MHSaveRecordNavigationController") as UINavigationController
+    //    self.presentViewController(saveRecordNC, animated: true, completion: nil)
+  }
+  
+  func registerNotification() {
+    let notification = UILocalNotification()
+    notification.fireDate = NSDate(timeIntervalSinceNow: self.datePicker.countDownDuration)
+    notification.alertBody = "請慢慢下座";
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    println("alarm ready")
   }
 }
 
